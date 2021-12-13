27ceed2b284d2db1bbe519a43061841356af0a08
nikita_kogolenok <nikiton17ns@gmail.com>
2021-12-13 12:05:40 +0300

    Delete storyboard and ViewController Add MainView

---
+++ WeatherTZ/MainController.swift
@@ -0,0 +1,12 @@
+//
+//  MainController.swift
+//  WeatherTZ
+//
+//  Created by Никита Коголенок on 13.12.21.
+//
+
+import UIKit
+
+class MainController: UIViewController {
+    
+}
--- WeatherTZ/SceneDelegate.swift
+++ WeatherTZ/SceneDelegate.swift
@@ -12,22 +12,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
-    }
-
-    func sceneDidDisconnect(_ scene: UIScene) {
-    }
-
-    func sceneDidBecomeActive(_ scene: UIScene) {
-    }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
+        guard let windowScene = scene as? UIWindowScene else { return }
+        //1
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        //2
+        window?.windowScene = windowScene
+        //3
+        window?.rootViewController = MainController()
+        //4
+        window?.makeKeyAndVisible()
     }
 }
 
